8365290c08fd3fe983ebbcab3be83b29027bed7b
Samuel Farto Vaamonde <sfartovaamonde@danielcastelao.org>
2022-02-08 14:10:51 +0100

    Boletín 23

--- Ejercicios/Ejercicio_9/Main.java
+++ Ejercicios/Ejercicio_9/Main.java
@@ -2,6 +2,8 @@ package Ejercicio_9;
 
 public class Main {
     public static void main(String[] args) {
+        ReemplazarVocales obx = new ReemplazarVocales();
+        obx.amosarReemplazo();
 
     }
 }
--- Ejercicios/Ejercicio_9/ReemplazarVocales.java
+++ Ejercicios/Ejercicio_9/ReemplazarVocales.java
@@ -1,9 +1,15 @@
 package Ejercicio_9;
 
+import javax.swing.*;
+
 public class ReemplazarVocales {
 
     public void amosarReemplazo(){
         String texto = "Jeve jeve jeve";
+        JOptionPane.showMessageDialog(null, texto);
+        String textoReemplazado;
+        textoReemplazado = texto.replace('e','a');
+        JOptionPane.showMessageDialog(null, " Texto reemplazado --> " + textoReemplazado);
 
     }
 }
