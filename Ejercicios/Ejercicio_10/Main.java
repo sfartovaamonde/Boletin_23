4969d302d69352da1825490c938c95b23c5ead1a
Samuel Farto Vaamonde <sfartovaamonde@danielcastelao.org>
2022-02-14 10:03:16 +0100

    Boletín 23

--- Ejercicios/Ejercicio_10/Main.java
+++ Ejercicios/Ejercicio_10/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
  Ascii obx = new Ascii();
  obx.amosarCodigoAscii();
+
+
+ //ESTE EJERCICIO ESTA MAL, HAY QUE CORREGIRLO!!!!!!!!!!!!!!!
     }
 }
